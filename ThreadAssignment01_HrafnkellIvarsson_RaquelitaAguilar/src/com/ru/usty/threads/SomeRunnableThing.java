84e91f9656805560624443a0a33723ba0f86adc0
Raquelita Rós Aguilar <raquelita@stokkur.is>
2017-02-02 00:05:58 +0000

    Implemented some part of the requirements. Requirement 1 is definitely correct, 2 and 3 need some improvements. But I think that's just minor thing.

--- ThreadAssignment01_HrafnkellIvarsson_RaquelitaAguilar/src/com/ru/usty/threads/SomeRunnableThing.java
+++ ThreadAssignment01_HrafnkellIvarsson_RaquelitaAguilar/src/com/ru/usty/threads/SomeRunnableThing.java
@@ -1,16 +1,23 @@
 package com.ru.usty.threads;
 
+// Create separate class that implements runnable
+// The function run is what will be executed when we call
+// someThread.start(); that is if the thread was implemented like this:
+// Thread someThread = new Thread(new SomeRunnableThing);
+
+
 public class SomeRunnableThing implements Runnable{
+	
+	Problem problem;
+	
 	@Override
 	public void run() {
-		long tID = Thread.currentThread().getId();
 		Solver.findAndPrintSolution(problem);
+		// long tID = Thread.currentThread().getId();
 		//System.out.println("ID: " + tID);
 	}
 	
 	public SomeRunnableThing(Problem p) {
 		problem = p;
 	}
-	
-	private static Problem problem;
 }
--- ThreadAssignment01_HrafnkellIvarsson_RaquelitaAguilar/src/com/ru/usty/threads/ThreadAssignment01Main.java
+++ ThreadAssignment01_HrafnkellIvarsson_RaquelitaAguilar/src/com/ru/usty/threads/ThreadAssignment01Main.java
@@ -1,44 +1,63 @@
 package com.ru.usty.threads;
+import java.util.concurrent.*;
 
 public class ThreadAssignment01Main {
 
     private static final int NUMBER_OF_PROBLEMS = 30;
+    // POOL_SIZE is the no. if threads we wish to run at a time
+    private static final int POOL_SIZE = 10;
 
     public static void main(String[] args) {
+
+    	
         System.out.println("Processors: " + Runtime.getRuntime().availableProcessors());
-        System.out.println("Solutions:");
-
         long startTime = System.currentTimeMillis();
-
-        // PROJECT REQUIREMENTS:
-        // 
-        // Write a program in Java that runs the function Solver.findAndPrintSolution() 30 times. 
-        // It will do it in the following three ways:
-        //
-        // 1. Sequentially: Don’t run the next instance until the one before has returned
-        //
-        // 2. All at once in separate threads. A new thread is created for each instance.
-        //
-        // 3. A certain number at a time. Threads are run through a thread pool of a certain size 
-        // (students can try different sizes). New instances aren’t run until a thread is free in the thread pool.
         
-        //Solver.findAndPrintSolution(Problematic.nextProblem());
-        
-        
+        Problem problem = Problematic.nextProblem();
+   
+        //                             REQUIREMENT 1
+        // Sequentially: Don’t run the next instance until the one before has returned
+        // Run the line "findAndPrint..." NUMBER_OF_PROBLEMS times
+        System.out.println("Requirement 1");
         for(int i = 0; i < NUMBER_OF_PROBLEMS; i++){
-        	Problem problem = Problematic.nextProblem();
-        	Thread someThread = new Thread(new SomeRunnableThing(problem));
-            someThread.start();
+            Solver.findAndPrintSolution(Problematic.nextProblem());
+        }
+        //-----------------------------------------------------------------------//
+
+        //                              REQUIREMENT 2
+        // All at once in separate threads. A new thread is created for each instance.
+        System.out.println("Requirement 2");
+        for(int i = 0; i < NUMBER_OF_PROBLEMS; i++){
+            new Thread(new SomeRunnableThing(problem)).start();
+        }
+        //-----------------------------------------------------------------------//
+
+        //                              REQUIREMENT 3
+        // A certain number at a time
+        // Threads are run through a thread pool of a certain size
+        // New instances aren’t run until a thread is free in the thread pool.
+        ExecutorService threadPool = Executors.newFixedThreadPool(POOL_SIZE);
+        System.out.println("Requirement 3");
+        for(int i = 0; i < NUMBER_OF_PROBLEMS; i++){
+            threadPool.execute(new SomeRunnableThing(problem));
+        }
+        threadPool.shutdown();
+
+        //-----------------------------------------------------------------------//
+
+
+        /*  for(int i = 0; i < NUMBER_OF_PROBLEMS; i++){
+        	    Problem problem = Problematic.nextProblem();
+        	    Thread someThread = new Thread(new SomeRunnableThing(problem));
+                someThread.start();
         	try {
 				someThread.join();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }
-        
-    	
-        System.out.println("All done");
+        }*/
+
         System.out.println("Total time: " + (System.currentTimeMillis() - startTime) + " ms");
     }
 }
